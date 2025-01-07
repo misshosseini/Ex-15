3319dc82b807bf96a7c1bbc1cb07c2c5c10687e7
mahsa hosseini <84369160+misshosseini@users.noreply.github.com>
2025-01-07 09:53:18 +0000

    update test_app

--- test_app.py
+++ test_app.py
@@ -10,10 +10,16 @@ class FlaskTestCase(unittest.TestCase):
     def test_home(self):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
-
+    
     def test_predict(self):
-        result = self.app.post('/predict', data=dict(MedInc=3.5, HouseAge=20, AveRooms=5))
+        result = self.app.post(
+            '/predict',
+            data=dict(MedInc=3.5, HouseAge=20, AveRooms=5)
+        )
         self.assertEqual(result.status_code, 200)
+        # بررسی محتوای پاسخ
+        self.assertIn('prediction', result.get_json())
+
 
 if __name__ == '__main__':
     unittest.main()
